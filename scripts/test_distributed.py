cff3e43da99ad19077884d349a58ab34619d900b
Kamil Piszczek <k4mil.piszczek@gmail.com>
2017-01-12 18:23:45 +0100

    Added test report generator. Test results

--- scripts/test_distributed.py
+++ scripts/test_distributed.py
@@ -6,25 +6,42 @@ root_path    = '../../ecg-classification/data/data_unique_labels'
 learn_cmd = '"../Program/bayes" -d "%s/train_data.txt" -d "%s/train_label.txt" -r -l -m -f ' + feature_mask
 test_cmd  = '"../Program/bayes" -d "%s/test_data.txt" -d "%s/test_label.txt" -t -m -f ' + feature_mask
 
+def Nan_handle(item):
+    if 'IND' in str(item):
+        item = 'NaN'
+    else:
+        item = float(item)
+    return item
+
 def bayes_call(learn_cmd, test_cmd):
     learn_proc = subprocess.Popen(learn_cmd, env=os.environ, stdout=subprocess.PIPE)
     learn_proc.wait()
     time_patt  = 'time:(.*)\['
     learn_time = float(re.findall(time_patt, learn_proc.stdout.read())[0])
+
     test_proc  = subprocess.Popen(test_cmd, env=os.environ, stdout=subprocess.PIPE)
     res  = test_proc.stdout.read()
-    return [re.findall('accuracy:(.*)%', res)[0],    \
-            re.findall('Sensitivity:(.*)%', res)[0], \
-            re.findall('Specificity:(.*)%', res)[0], \
-            str(learn_time + float(re.findall(time_patt, res)[0]))]
+
+    return [float(re.findall('accuracy:(.*)%', res)[0]),    \
+            Nan_handle(re.findall('Sensitivity:(.*)%', res)[0]), \
+            Nan_handle(re.findall('Specificity:(.*)%', res)[0]), \
+            (learn_time + float(re.findall(time_patt, res)[0]))]
 
 # Package label: Accuracy | Sensitivity | Specificity | Execution time
 def report_generate(data_label, data):
-    output = str(data_label)
-    output += ": "
+    output =  str('\t\t\\hline\n\t\t')
+    output += str(data_label)
+    output += " & "
     gap_width = 10
     for item in data:
-        output += str(item) + (gap_width - len(str(item)))*" "
+        float_str = str(item)
+        if isinstance(item, float):
+            float_str = '%3.2f' % item
+        output += str(float_str) + (gap_width - len(str(float_str)))*" "
+        if item != data[-1]:
+            output += ' & '
+        else:
+            output += ' \\\\'
     return output
 
 if __name__ == "__main__":
